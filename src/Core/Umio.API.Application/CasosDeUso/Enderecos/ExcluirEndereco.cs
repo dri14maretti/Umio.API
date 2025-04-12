a1ec44009827e43bc3d451e59d987864eb4c3e14
Adriano Maretti <dri.cmaretti@gmail.com>
2025-04-12 19:41:29 -0300

    adicionando funcionalidade de excluir endereõ

---
+++ src/Core/Umio.API.Application/CasosDeUso/Enderecos/ExcluirEndereco.cs
@@ -0,0 +1,23 @@
+﻿using Umio.API.Application.CasosDeUso.Enderecos.Interfaces;
+using Umio.API.Application.Contratos.Repositorios;
+
+namespace Umio.API.Application.CasosDeUso.Enderecos
+{
+    internal class ExcluirEndereco : IExcluirEndereco
+    {
+        private readonly IEnderecoRepository _enderecoRepository;
+        public async Task<bool> Executar(Guid id)
+        {
+            var endereco = await _enderecoRepository.BuscarEnderecoPorId(id);
+
+            if(endereco == null)
+            {
+                throw new Exception("Endereço não encontrado");
+            }
+
+            await _enderecoRepository.ExcluirEndereco(id);
+
+            return true; // Exemplo de retorno, substitua pela lógica real
+        }
+    }
+}
---
+++ src/Core/Umio.API.Application/CasosDeUso/Enderecos/Interfaces/IExcluirEndereco.cs
@@ -0,0 +1,7 @@
+﻿namespace Umio.API.Application.CasosDeUso.Enderecos.Interfaces
+{
+    public interface IExcluirEndereco
+    {
+        public Task<bool> Executar(Guid id)
+    }
+}
