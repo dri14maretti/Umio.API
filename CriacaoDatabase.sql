78adafab3a1c94c9379386a2ae74101d630ac09c
Adriano Maretti <dri.cmaretti@gmail.com>
2025-05-05 21:19:53 -0300

    alterações script de criação

--- CriacaoDatabase.sql
+++ CriacaoDatabase.sql
@@ -1,32 +1,28 @@
--- Criar database UMIO antes de executar script
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Tabela TipoCupom
 CREATE TABLE TipoCupom (
     Id SERIAL PRIMARY KEY,
     Nome VARCHAR(255) NOT NULL
 );
 
--- Tabela Cupom
 CREATE TABLE Cupom (
     Codigo VARCHAR(50) PRIMARY KEY,
-    Ativo BOOLEAN DEFAULT TRUE,
+    Status VARCHAR(50),
     Porcentagem DECIMAL,
     TipoCupomId INT,
     FOREIGN KEY (TipoCupomId) REFERENCES TipoCupom(Id)
 );
 
--- Tabela Cliente
 CREATE TABLE Cliente (
-    Id UUID PRIMARY KEY,
+    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Nome VARCHAR(255) NOT NULL,
     Email VARCHAR(255) UNIQUE NOT NULL,
     Telefone VARCHAR(20),
     Pontos INT DEFAULT 0
 );
 
--- Tabela Endereco
 CREATE TABLE Endereco (
-    Id UUID PRIMARY KEY,
+    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Cep VARCHAR(10),
     Rua VARCHAR(255),
     Bairro VARCHAR(255),
@@ -37,80 +33,112 @@ CREATE TABLE Endereco (
     FOREIGN KEY (ClienteId) REFERENCES Cliente(Id)
 );
 
-CREATE TABLE Provedor(
+CREATE TABLE Provedor (
 	Id SERIAL PRIMARY KEY,
-	Provedor VARCHAR(50)
+	Nome VARCHAR(50) NOT NULL
 );
 
--- Tabela Usuario
 CREATE TABLE Usuario (
-    Id UUID PRIMARY KEY,
+    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     ProvedorId INT,
-    ClienteId UUID,
-    Senha VARCHAR(255) NOT NULL,
+    ClienteId UUID UNIQUE,
+    Senha VARCHAR(255),
     FOREIGN KEY (ClienteId) REFERENCES Cliente(Id),
 	FOREIGN KEY (ProvedorId) REFERENCES Provedor(Id)
 );
 
--- Tabela FormaPagamento
 CREATE TABLE FormaPagamento (
     Id SERIAL PRIMARY KEY,
     Nome VARCHAR(255) NOT NULL
 );
 
--- Tabela dos status possíveis do pedido
 CREATE TABLE StatusPedido (
 	Id SERIAL PRIMARY KEY,
-	Status VARCHAR(50)
+	Status VARCHAR(50) NOT NULL
 );
 
--- Tabela Pedido
+CREATE TABLE CategoriaProduto (
+    Id SERIAL PRIMARY KEY,
+    Categoria VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Produto (
+    Id SERIAL PRIMARY KEY,
+    Nome VARCHAR(255) NOT NULL,
+    Preco DECIMAL NOT NULL,
+    Descricao TEXT,
+    CategoriaId INT,
+    Imagem VARCHAR(255),
+    TipoProduto VARCHAR(50),
+    FOREIGN KEY (CategoriaId) REFERENCES CategoriaProduto(Id)
+);
+
+CREATE TABLE Adicional (
+    Id SERIAL PRIMARY KEY,
+    Nome VARCHAR(255) NOT NULL,
+    Valor DECIMAL NOT NULL
+);
+
+CREATE TABLE CategoriaAcompanhamento (
+    Id SERIAL PRIMARY KEY,
+    Categoria VARCHAR(255) NOT NULL,
+    MaximoGratis INT DEFAULT 0
+);
+
+CREATE TABLE Acompanhamento (
+    Id SERIAL PRIMARY KEY,
+    Nome VARCHAR(255) NOT NULL,
+    Valor DECIMAL NOT NULL,
+    CategoriaAcompanhamentoId INT,
+    FOREIGN KEY (CategoriaAcompanhamentoId) REFERENCES CategoriaAcompanhamento(Id)
+);
+
 CREATE TABLE Pedido (
-    Id INT PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     EnderecoId UUID,
     ClienteId UUID,
     Comentarios TEXT,
     ValorEntrega DECIMAL,
     Total DECIMAL NOT NULL,
-    Status INT,
+    StatusId INT,
     PontosAcumulados INT DEFAULT 0,
     CodigoCupom VARCHAR(50),
     FormaPagamentoId INT,
     FOREIGN KEY (EnderecoId) REFERENCES Endereco(Id),
     FOREIGN KEY (ClienteId) REFERENCES Cliente(Id),
+    FOREIGN KEY (StatusId) REFERENCES StatusPedido(Id),
     FOREIGN KEY (CodigoCupom) REFERENCES Cupom(Codigo),
-    FOREIGN KEY (FormaPagamentoId) REFERENCES FormaPagamento(Id),	
-	FOREIGN KEY (Status) REFERENCES StatusPedido(Id)
+    FOREIGN KEY (FormaPagamentoId) REFERENCES FormaPagamento(Id)
 );
 
-
--- Tabela Produto
-CREATE TABLE Produto (
-    Id INT PRIMARY KEY,
-    Nome VARCHAR(255) NOT NULL,
-    Preco DECIMAL NOT NULL,
-    Descricao TEXT,
-    Categoria VARCHAR(255),
-    Imagem VARCHAR(255),
-    TipoProduto VARCHAR(50),
-    MelhoriasAdicionais TEXT,
-    MelhoriasMolhos TEXT,
-    MelhoriasAcompanhamentos TEXT
-);
-
--- Tabela CupomProduto (Tabela Associativa para a relação N:N entre Cupom e Produto)
-CREATE TABLE CupomProduto (
-    CodigoCupom VARCHAR(50),
-    ProdutoId INT,
-    FOREIGN KEY (CodigoCupom) REFERENCES Cupom(Codigo),
-    FOREIGN KEY (ProdutoId) REFERENCES Produto(Id)
-);
-
--- Tabela PedidoProduto (Tabela Associativa para a relação N:N entre Pedido e Produto)
-CREATE TABLE PedidoProduto (
+CREATE TABLE PedidoXProduto (
+    Id SERIAL PRIMARY KEY,
     PedidoId INT,
     ProdutoId INT,
     Comentario TEXT,
+    Quantidade INT NOT NULL DEFAULT 1,
     FOREIGN KEY (PedidoId) REFERENCES Pedido(Id),
+    FOREIGN KEY (ProdutoId) REFERENCES Produto(Id)
+);
+
+CREATE TABLE AdicionalProdutoPedido (
+    ProdutoPedidoId INT,
+    AdicionalId INT,
+    FOREIGN KEY (ProdutoPedidoId) REFERENCES PedidoXProduto(Id),
+    FOREIGN KEY (AdicionalId) REFERENCES Adicional(Id)
+);
+
+CREATE TABLE AcompanhamentoProdutoPedido (
+    ProdutoPedidoId INT,
+    AcompanhamentoId INT,
+    FOREIGN KEY (ProdutoPedidoId) REFERENCES PedidoXProduto(Id),
+    FOREIGN KEY (AcompanhamentoId) REFERENCES Acompanhamento(Id)
+);
+
+CREATE TABLE CupomProduto (
+    CodigoCupom VARCHAR(50),
+    ProdutoId INT,
+    PRIMARY KEY (CodigoCupom, ProdutoId),
+    FOREIGN KEY (CodigoCupom) REFERENCES Cupom(Codigo),
     FOREIGN KEY (ProdutoId) REFERENCES Produto(Id)
 );
